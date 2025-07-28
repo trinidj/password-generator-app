cbf06302e3dcca3e507604724f44a7320e4d1cd3
trinidj <jm.trinidad199@gmail.com>
2025-07-28 13:28:52 -0400

    style: fix indentation in Button component styles

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -6,14 +6,14 @@
 
 <style scoped>
   .button {
-  display: flex;
-  padding: 10px;
-  border: none;
-  border-radius: 10px;
-  align-items: center;
-  justify-content: center;
-  transition: all 0.1s ease;
-}
+    display: flex;
+    padding: 10px;
+    border: none;
+    border-radius: 10px;
+    align-items: center;
+    justify-content: center;
+    transition: all 0.1s ease;
+  }
 
   .button:hover {
     cursor: pointer;
