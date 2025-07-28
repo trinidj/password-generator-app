904bd4abf123ac22c5ff4745ff6ee05716c53ec7
trinidj <jm.trinidad199@gmail.com>
2025-07-28 13:29:35 -0400

    feat: add PasswordDisplay component

---
+++ src/components/ui/PasswordDisplay.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <input type="text" placeholder="Click to generate password" readonly />
+</template>
+
+<style scoped>
+  input {
+    border: none;
+    padding: var(--spacing-base);
+    background: var(--base-color);
+  }
+
+  input:focus {
+    outline: none;
+  }
+</style>
