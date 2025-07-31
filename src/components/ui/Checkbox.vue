328e3ea108701fa86c2af288fb2ba0c6458e5be4
trinidj <jm.trinidad199@gmail.com>
2025-07-31 19:28:37 -0400

    feat: add Checkbox UI component

---
+++ src/components/ui/Checkbox.vue
@@ -0,0 +1,29 @@
+<script setup>
+  const props = defineProps({
+    title: {
+      type: String
+    }
+  })
+</script>
+
+<template>
+  <div class="checkbox-container">
+    <input type="checkbox" />
+    <p>{{ props.title }}</p>
+  </div>
+</template>
+
+<style scoped>
+  .checkbox-container {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .checkbox-container input {
+    padding: var(--spacing-lg);
+  }
+
+  .checkbox-container p {
+    color: var(--text-color);
+  }
+</style>
